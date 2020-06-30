9c66c06bd596b117773e7b0007dcc296693ef41c
Gregg Whitworth <whitwort@gmail.com>
2020-06-30 19:47:49 -0400

    code cleaup; initial implementation of launchInstance missing block device mounting.

--- R/instances.R
+++ R/instances.R
@@ -9,17 +9,16 @@
 #' This function is designed to be as simple as possible and does not support
 #' the vast majority of configuration options available when launching EC2
 #' instances.  If it doesn't meet your needs see the \code{paws} package for a
-#' full featured EC2 API.  You can pass the return value from the
-#' \code{paws.compute} \code{run_instances} function to the
-#' \code{\link{parseLaunchResponse}} to get an \code{instance} object to use
-#' with the other functions in this package.
+#' full featured EC2 SDK  You can use the \code{paws::ec2()$run_instances}
+#' function to launch your instance and then pass the `InstanceID` to
+#' \code{link{getInstanceDescription}} function to generate an \code{instance}
+#' object to use with the other functions in this package.
 #'
-#' @param imageID The image-id for the AMI used to launch the instance.
+#' @param imageId The image-id for the AMI used to launch the instance.
 #' @param keyName The key-pair on your AWS account to use.  This pair should be
 #'   associated with one of the default public keys on the current account that
 #'   \code{ssh} will try to use when connecting to your instance.
-#' @param securityGroups The EC2 security group names to use.  This function
-#'   launches your instance with the EC2-Classic scheme, not using VPC.
+#' @param securityGroupIds The EC2 security group(s) Id(s) to use.
 #' @param instanceType The type of instance to launch.
 #' @param tags Tags to apply to your instance and its resources. This should
 #'   either be NULL or a normal R list with named elements; names will be tag
@@ -27,25 +26,195 @@
 #' @param blockDeviceMapping Additional block devices to create and mount. This
 #'   should either be NULL or an object returned by the
 #'   \code{\link{blockDeviceMapping}} function.
+#' @param throttle The amount of time to wait (in seconds) before repeating
+#'   requests to the AWS API.  The account-wide limit is 10,000 r/s, so this
+#'   default is extremely conservative.
+#' @param username The administrator username to use when logging in over ssh.
+#'   Default is set for Amazon Linux AMIs.
+#' @param sshTimeout How many times to re-test connecting over ssh after
+#'   recieving a timeout error.
 #'
 #' @return An \code{instance} object that can be used with other functions in
 #'   this package that need to communicate with your launched instance.
 #' @export
 #' 
-launchInstance <- function( imageID
+launchInstance <- function( imageId
                           , keyName
-                          , securityGroups
+                          , securityGroupIds
                           , instanceType       = "t2.micro"
                           , tags               = NULL
                           , blockDeviceMapping = NULL
+                          , throttle           = 1
+                          , username           = "ec2-user"
+                          , sshTimeout         = 10
                           ) {
+  
+  formattedTags <- lapply( names(tags)
+                          , function(key) {
+                              list(Key = key, Value = tags[[key]])
+                            }
+                          )
+  
+  # get the paws::EC2 service
+  ec2  <- paws::ec2()
+  
+  # check that AWS API access is configured
+  cat("Checking AWS API access to EC2...")
+  tryCatch( ec2$describe_instances
+          , error = function(e) {
+              cat( crayon::red(" failed.\n")
+                 , "There was an error trying to use the `paws` package to connect to the AWS API.  This is probably because you have not setup your AWS configuration and credentials.  Please see the 'configuration' vignette for instructions. Error message:"
+                 )
+              stop(e)
+            }
+          )
+  cat(crayon::green(" done.\n"))
+  
+  # use paws to launch the instance; this blocks until the instance is created.
+  cat("Launching EC2 instance...")
+  resp <- ec2$run_instances( ImageId = imageId
+                           , KeyName = keyName
+                           , SecurityGroupIds = as.list(securityGroupIds)
+                           , InstanceType = instanceType
+                           , TagSpecifications = list(
+                               list(
+                                 ResourceType = "instance",
+                                 Tags         = formattedTags
+                               )
+                             )
+                           , MaxCount = 1
+                           , MinCount = 1
+                           )
+  cat(crayon::green(" done.\n"))
+  
+  # get the instanceId from the response data
+  instanceId <- resp$Instances[[1]]$InstanceId
+  cat("The instance id is: ", crayon::cyan(instanceId), "\n")
+  
+  instance <- getInstanceDescription(instanceId, throttle)
+  
+  ## NB: the AWS API describe_instance_status function reports on whether or not
+  ## the System and Interface are reachable.  However these checks are slow to
+  ## update and SSH appears to be up and running long before these checks
+  ## *actually* pass.  Therefore the currently implementation is to just try to
+  ## connect with ssh after the state turns to "running" rather than wait for
+  ## those tests to pass.  This may be a bad idea.
+  
+  # check ssh access
+  host <- paste0(username, "@", instance$ip)
+  Sys.sleep(5)
+  cat("Checking ssh connection (", crayon::cyan(host), ")...\n")
+  sshTimeouts <- 0
+  while (!trySSH(host)) {
+    if (sshTimeouts == sshTimeout) {
+      cat( crayon::red(" failed.\n\n")
+         , "Your instance is running but an error occured when trying to connect to your instance over ssh; be sure to terminate it using 'terminateInstance', the web console or AWS CLI!  It may be that we just have to be more patient for your server to get up and running (try increasing the value of sshTimeout).  Also check to make sure that sshd is installed and configured on your AMI and that the firewall is setup corretly through your security group.  Try debugging the problem by running ssh in verbose mode:\n\n$ssh -v ", host
+         )
+      return(instance)
+    }
+    
+    Sys.sleep(throttle)
+    sshTimeouts <- sshTimeouts + 1
+  }
+  
+  cat(crayon::green("done.\n"))
+  cat(crayon::green("\nSuccess!"), " Your instance is now ready to use.")
+  
+  instance
+}
+
+trySSH <- function(host) {
+  tryCatch( { suppressMessages(session <- ssh::ssh_connect(host))
+              ssh::ssh_disconnect(session)
+              TRUE
+            }
+          , error = function(e) { 
+              if (grepl("Timeout", e$message, ignore.case = TRUE) | grepl("refused", e$message, ignore.case = TRUE)) {
+                return(FALSE)
+              } else {
+                cat( crayon::red(" failed.\n\n")
+                   , "An error occured when trying to connect to your instance over ssh.  If your permissions were denied you may not have your local ssh keys setup corretly.  See the 'configuration' vignette for instructions.  Try debugging the problem by running ssh in verbose mode:\n\n$ssh -v ", host, "\n\nError message:\n\n"
+                   )
+                stop(e)
+              }
+            }
+           )
+}
+
+#' Gather descriptive information about a running instance
+#'
+#' This function checks to make sure a launched instance is Running and both the
+#' System and Instance reachability tests have passed, then returns an instance
+#' description used by the other functions in this package.  This function is
+#' useful if you want to launch instances yourself using the Web console, AWS
+#' CLI, or paws SDK instead of using \code{\link{launchInstance}}.
+#'
+#' @param instanceId The InstanceID returned or displayed through whatever
+#'   mechanism used to launch the EC2 instance
+#' @param throttle The amount of time to wait (in seconds) before repeating
+#'   requests to the AWS API.  The account-wide limit is 10,000 r/s, so this
+#'   default is extremely conservative.
+#'
+#' @return An \code{instance} object used by the other functions in this package
+#' @export
+#'
+getInstanceDescription <- function(instanceId, throttle = 1) {
+
+  # wait for the instance State to be running
+  cat("Waiting for the instance to be Running...")
+  while(!isRunning(instanceId)) {
+    Sys.sleep(throttle)
+  }
+  cat(crayon::green(" done.\n"))
+
+  # The current setup assumes all needed description fields are populated by the
+  # time the state turns to "running".  So far so good.
+  cat("Getting instance description...")
+  resp <- getDescription(instanceId)
+  cat(crayon::green(" done.\n"))
+  
+  list( id = instanceId
+      , ip = resp$Reservations[[1]]$Instances[[1]]$PublicIpAddress
+      )
+}
+
+isRunning <- function(instanceId) {
+  tryCatch({
+      ec2   <- paws::ec2()
+      resp  <- ec2$describe_instances(InstanceIds = instanceId)
+      state <- resp$Reservations[[1]]$Instances[[1]]$State$Name
+      state == "running"
+    }
+  , error = function(e) {
+      cat(crayon::red(" failed.\n\n"), connectionError(instanceId))
+      stop(e)
+    }
+  )
   
 }
 
-#' Describe one or more block devices to create and mount on an EC2 instance.
+getDescription <- function(instanceId) {
+  tryCatch({
+      ec2 <- paws::ec2()
+      ec2$describe_instances(InstanceIds = instanceId)
+    }
+  , error = function(e) { 
+      cat(crayon::red(" failed.\n\n"), connectionError(instanceId))
+      stop(e)
+    }
+  )
+}
+
+connectionError <- function(instanceId) {
+  cat("An error occured when trying to connect to your EC2 instance or check its status.  Please check the instance state using the Web console or AWS-cli as it may still be running; the instance InstanceId is ", crayon::cyan$bold(instanceId), ". Error message:")
+}
+
+#' Describe one or more block devices (EBS) to create and mount on an EC2 instance.
 #'
 #' The arguments to this function are vectorized, so you can use it to describe
-#' one or more block devices to create and mount.
+#' one or more block devices to create and mount.  Pay particular care to the
+#' \code{deleteOnTermination} argument!  If set incorrectly you may unexpectedly
+#' lose data or incur EBS charges.
 #'
 #' @param deviceName A character vector giving the names of one or more block
 #'   devices to create on instance launch.  The length of \code{mountDir} must
@@ -53,8 +222,11 @@ launchInstance <- function( imageID
 #' @param volumeSize A numeric vector giving the size in GB of the volume(s) to
 #'   create.
 #' @param deleteOnTermination TRUE or FALSE to determine if the volume is
-#'   deleted when the associated EC2 instance is terminated (NOTE: the volume
-#'   will NOT be deleted if the EC2 instance is stopped instead of terminated).
+#'   deleted when the associated EC2 instance is terminated. \bold{WARNING #1}:
+#'   Even if set to TRUE, the volume will not be deleted if the attached EC2
+#'   instance is stopped instead of terminated.  \bold{WARNING #2}:  If you set
+#'   this to FALSE than you will continue to incur EBS charges for data on this
+#'   volume even after the instance has been terminated.
 #' @param mountDir The path on the instance file system where the device(s)
 #'   should be mounted after the instance starts up.  The length of this vector
 #'   must math the length of \code{deviceName}.
@@ -62,7 +234,7 @@ launchInstance <- function( imageID
 #' @return A list of block device mappings that can be used with
 #'   \code{\link{launchInstance}}.
 #' @export
-#'
+#' 
 blockDeviceMapping <- function( deviceName          = "/dev/sdb"
                               , volumeSize          = 8
                               , deleteOnTermination = TRUE
@@ -95,7 +267,7 @@ blockDeviceMapping <- function( deviceName          = "/dev/sdb"
 #' EC2 instance, flags them as executable, and then runs them.
 #'
 #' @param instance An instance objected created with
-#'   \code{\link{launchInstance}}.
+#'   \code{\link{launchInstance}} or \code{\link{getInstanceDescription}}.
 #' @param launchScript The name of the launch script file created by
 #'   \code{\link{makeLaunchScript}}.
 #' @param scriptPath Path to the folder where jobs scripts and the launch script
@@ -111,45 +283,41 @@ launchJobs <- function( instance
   
 }
 
-
 #' Check whether or not an EC2 instance is still running
 #'
 #' @param instance An instance objected created with
-#'   \code{\link{launchInstance}}.
+#'   \code{\link{launchInstance}} or \code{\link{getInstanceDescription}}.
 #'
-#' @return TRUE or FALSE
+#' @return TRUE or FALSE.  All conditions except the "running" state return
+#'   FALSE (including AWS API and connection errors).
 #' @export
-#'
+#' 
 checkAlive <- function(instance) {
-  
+  tryCatch( isRunning(instance)
+          , error = function(e) { FALSE }
+          )
 }
 
 #' Get the status of jobs launched on an EC2 instance.
 #'
-#' @param instance 
+#' @param instance An instance objected created with
+#'   \code{\link{launchInstance}} or \code{\link{getInstanceDescription}}.
 #'
 #' @return
 #' @export
 #'
-#' @examples
 getStatus <- function(instance) {
   
 }
 
-#' Parses an AWS run-instances response into an instance object
+#' Title
 #'
-#' This function converts the return value from the \code{paws.compute::ec2}
-#' \code{run_instances} function into an \code{instance} object used by the
-#' other functions in this package.  Normally you would never have to call this
-#' function directly, but it is available if you need to create instances using
-#' \code{paws.compute::ec2}.
+#' @param instance An instance objected created with
+#'   \code{\link{launchInstance}} or \code{\link{getInstanceDescription}}.
 #'
-#' @param resp The response data structure returned by EC2 run-instances.
-#'
-#' @return An \code{instance} object that can be used with the other functions
-#'   in this package.
+#' @return
 #' @export
 #'
-parseLaunchResponse <- function(resp) {
+terminateInstance <- function(instance) {
   
 }
--- README.md
+++ README.md
@@ -2,27 +2,28 @@
 
 This package contains a set of R tools designed to ease and automate deploying data analysis projects to AWS EC2 instances.
 
-## Limitations
+## Installation & configuration
 
-The goal for this package is to provide a highly simplified workflow for straightforward projects.  See the [paws](https://github.com/paws-r/paws) package if you need a much more complete AWS SDK for R.  These tools don't handle resource management or load balancing between instances so they are only suitable for projects where resource needs are easy to determine ahead of time; see a project like [Slurm](https://github.com/SchedMD/slurm) if you need a full featured job scheduler to manage a compute cluster.
-
-## Dependencies
-
-Many of the functions in this package make system calls and assume a standard Linux environment on the local machine but should work fine through most Bash on Windows distributions and *may* work on MacOS, depending on what tools are installed. 
-
-Finally, this toolset has been developed and tested on EC2 instances running [Amazon Linux 2](https://aws.amazon.com/amazon-linux-2/), but should work with any standard Linux distribution.
-
-## Installation & Setup
-
-To install from GitHub first make sure you have the `devtools` package installed, then run:
+To install this package from GitHub first make sure you have `devtools` installed, then run:
 
 ```r
-devtools::install_github('whitwort/AWSTools')
+devtools::install_github('whitwort/AWSTools', build_vignettes = TRUE)
 ```
 
-To use this package you will need to configure your AWS user credential files (or set environment variables).  As an example, you can do this using the [aws-cli](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html) `aws configure` command.  You will also need to make sure that the `ssh` key-pair you use to launch your EC2 instance is among the default keys that `ssh` will try when attempting to connect to the instance.
+To use this package you will need to configure your AWS user credentials.  As an example, you can do this using the [aws-cli](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html) `aws configure` command.  Please see the `paws` credentials documentation for more options (**note**: using a custom service config is not currently compatible with this package).
+
+You will also need to make sure that the `ssh` key-pair you use to launch your EC2 instance is among the default keys that `ssh` will try when attempting to connect to the instance.
+
+For more detailed configuration instructions see the [configuration](https://github.com/whitwort/AWSTools/blob/master/vignettes/configuration.html) vignette.
+
+## Limitations
+
+The goal for this package is to provide a highly simplified workflow for straightforward projects.  See the [paws](https://github.com/paws-r/paws) package if you need a much more thorough AWS SDK for R.  
+
+These tools don't handle resource management or load balancing between instances so they are only suitable for projects where resource needs are easy to determine ahead of time. See a project like [Slurm](https://github.com/SchedMD/slurm) if you need a full featured job scheduler to manage a compute cluster.
+
+Finally, this toolset has been developed and tested on EC2 instances running [Amazon Linux 2](https://aws.amazon.com/amazon-linux-2/), but it *should* work with any standard Linux distribution.  It is not designed to work with Windows instances.
 
 ## License
 
 Copyright © 2020 Gregg Whitworth and licensed under [GPLv3](https://www.gnu.org/licenses/gpl-3.0.en.html).
-
